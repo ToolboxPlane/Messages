cc0eeea0233aeaad73588a20be9f767acab2885f
Paul Nykiel <paul.nykiel@gmail.com>
2022-11-02 18:53:12 +0100

    Added conversion functions

---
+++ MessageIds.h
@@ -0,0 +1,15 @@
+/**
+ * @file MessageIds.h
+ * @author paul
+ * @date 23.10.22
+ * Description here TODO
+ */
+#ifndef FLIGHTCOMPUTER_MESSAGEIDS_H
+#define FLIGHTCOMPUTER_MESSAGEIDS_H
+
+#define FC_ID 0x10
+#define SBUS_ID 0x20
+#define PDB_ID 0x30
+#define FC_SP_ID 0x40
+
+#endif // FLIGHTCOMPUTER_MESSAGEIDS_H
