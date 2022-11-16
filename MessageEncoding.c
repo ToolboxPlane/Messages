7e41f490badd48b6edae98946b06da08c9af57c0
Paul Nykiel <paul.nykiel@gmail.com>
2022-11-16 19:41:55 +0100

    Format

--- MessageEncoding.c
+++ MessageEncoding.c
@@ -5,9 +5,10 @@
  * Description here TODO
  */
 #include "MessageEncoding.h"
+
 #include "MessageDefs.hpp"
 
-uint16_t message_encode(uint8_t  *buf, uint16_t size, const pb_msgdesc_t *fields, const void *message, uint8_t id) {
+uint16_t message_encode(uint8_t *buf, uint16_t size, const pb_msgdesc_t *fields, const void *message, uint8_t id) {
     pb_ostream_t ostream = pb_ostream_from_buffer(buf + 2, size - 3);
     pb_encode(&ostream, fields, message);
     buf[0] = START_BYTE;
